4d63f1d8e0c4bb427c2e64297ce10d7ece3f35dc
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-07-11 22:25:36 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // app.js
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors'); 
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
 const app = express();
 connectDB();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/produtos', productRoutes);
